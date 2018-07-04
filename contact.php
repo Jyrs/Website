9848f0c00d025bb388143d997cddeb492f232687
Jyrs <gj-krsnv@ya.ru>
2018-07-04 04:21:07 +0300

    add contact.php

---
+++ contact.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>GJ SOFTWARE</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<link href = "style.css" rel = "stylesheet" type = "text/css" >
+	<link rel="stylesheet" href="css/animate.css">
+
+</head>
+
+<body>
+  <header>
+    	<a href= "index.php"> <img class="logot" src="img/logo.png" width="595px" height="240px" align="center"></a>
+  </header>
+
+
+	<div class="row-fluid">
+		<div class= "col-md-2"></div>
+
+			<div class= "col-md-1">
+				<ul class="animated wow fadeInLeft nav nav-tabs nav-stacked menu">
+          <li><a href="index.php">Главная</a></li>
+					<li><a href="">Блог</a></li>
+          <li><a href="AboutUS.php">О нас</a></li>
+					<li><a href="services.php">Услуги</a></li>
+					<li><a href="#">Портфолио</a></li>
+					<li><a href="contact.php">Контакты</a></li>
+				</ul>
+			</div>
+
+		<div class= "wow animated fadeInUp col-md-6"	data-wow-duration="1s" >
+			<table id = "table" border="0" width="100%" height="100%"  cellpadding="10" align="center">
+				<tr><th><p>	Тут контакты <p>	</th></tr></table>
+		</div>
+	</div>
+
+<footer class= " col-md-12 footer">
+		<p class = "col-xs-12 col-sm-12 col-md-12 col-lg-12 Contacts" align = "right"> gj-krsnv@ya.ru <br> gj-buldakov@ya.ru </br><p>
+			<section class= "col-xs-12 col-sm-12 col-md-12 col-lg-12 copyright" align = "center" >&copy; GJ SOFTWARE 2018</section>
+</footer>
+
+
+</body>
+
+<script src="js/wow.min.js"></script>
+<script>
+    new WOW().init();
+</script>
+
+</html>
