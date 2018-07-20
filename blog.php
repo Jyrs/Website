bdf6638d52001ad5ffd0bf13b0835437881f028f
Jyrs <gj-krsnv@ya.ru>
2018-07-20 14:38:48 +0300

    Адаптивный под БД блог

--- blog.php
+++ blog.php
@@ -21,7 +21,14 @@
 <body>
 
 
-  	<?php include "includes/header.php"; ?>
+  	<?php include "includes/header.php";
+				 	$tags_q = mysqli_query($connection, "SELECT * FROM `tagss`");
+					$tags = array();
+					while($tags_result = mysqli_fetch_assoc($tags_q))
+					{
+						$tags[] = $tags_result;
+					}
+		?>
 
 
 	<div class="row-fluid">
@@ -34,16 +41,52 @@
 		<div class= "wow animated fadeInUp col-xs-12 col-sm-12 col-md-6"	data-wow-duration="1s" >
 			<table id = "table" border="0" width="100%" height="100%"  cellpadding="10" align="center">
 				<tr><th>
-          <p>	Тут блог <p>
+						<h3 style="margin:10px;color: #d1d1d1;">Новое:</h3>
+					<?php $article = mysqli_query($connection, "SELECT * FROM `Articles`"); ?>
+					<?php while($art_res = mysqli_fetch_assoc($article))
+					{
+					?>
+					<article class = "article">
+
+						<div class ="article_image" style=" background:
+						url(img/<?php echo $art_res['img'];?>); "></div>
+						<div class ="article_info">
+							<h4 class="article_name"><a href="/article.php?id=<?php echo $art_res['id']; ?>"><?php echo $art_res['titles'];?></a></h4>
+								<div class ="article_info_meta">
+									<?php foreach($tags as $tags_result)
+									{
+										if($art_res['categorie_id'] == $tags_result['id']) {$art_tag = $tags_result; break;}
+									}
+									?>
+
+
+									<small>Теги: <a class ="tags_des" style="font-size: 10pt;" href ="/categorie.php?id=<?php echo $art_tag['id'];?>"> <?php echo $art_tag['titles'];?> </a></small>
+								</div>
+								<div class ="article_info_preview"><?php echo mb_substr($art_res['text'],
+								0, 200, 'utf-8');?>
+
+								</div>
+					</div>
+						<div class="data">
+							<small><?php echo $art_res['pubdate'];?></small>
+						</div>
+
+					<hr style="margin-right: 10px; margin-left: 10px;">
+					 </article>
+
+					 <?php
+				 	}
+					?>
+
         </th></tr></table>
 		</div>
 
 			<div class = "wow animated fadeInRight col-xs-12 col-sm-12 col-md-3 tags" data-wow-duration="1s">
-				<?php $tags = mysqli_query($connection, "SELECT * FROM `tagss`");
- 							while($tags_result = mysqli_fetch_assoc($tags))
+				<?php
+ 							foreach($tags as $tags_result)
 						{
 							?>
-							<a  class="tags_des" href="#"><?php	echo $tags_result['titles']; ?><a>
+							<a  class="tags_des" href="/categorie.php?id=<?php echo $tags_result['id']; ?>"><?php	echo $tags_result['titles']; ?><a>
 						<?php
 						}
 						?>
